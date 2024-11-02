a9f331be76f1c2ad015742cccac44a269853f7df
Salah Obied <117477274+SalBied@users.noreply.github.com>
2024-11-02 21:19:12 +0300

    Update chat.php

--- app/Broadcasting/chat.php
+++ app/Broadcasting/chat.php
@@ -19,9 +19,7 @@ class chat
      */
     public function join(User $user, \App\Models\Chat $chat): array|bool
     {
-        dd(2);
-        logger('f u b a');
-        // Only allow the buyer or the seller in the chat to listen to this channel
+   
         return $chat && ($chat->buyer_id === $user->id || $chat->seller_id === $user->id);
 
     }
