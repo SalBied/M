50b3c464a6664bc17a9c1be2ee02dbf2bb1e86f2
Salah Obied <117477274+SalBied@users.noreply.github.com>
2024-11-02 21:17:19 +0300

    Update channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -6,15 +6,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Broadcast::channel('chat.{chatId}', function ($user, $chatId) {
-   dd(2);
     $chat = Chat::find($chatId);
-    logger('f u b a');
     // Only allow the buyer or the seller in the chat to listen to this channel
     return $chat && ($chat->buyer_id === $user->id || $chat->seller_id === $user->id);
 });
 
-Broadcast::channel('c', function () {
-    logger('f u b');
-    return false; // Public channel, always allow subscription
-});
-Broadcast::channel('chats.{chat}', \App\Broadcasting\chat::class);
+
